2a096af1f09cf71fe022bd765da6625c4a0efa90
emarubi <69473685+emarubi@users.noreply.github.com>
2021-04-09 16:13:23 +0200

    Class ans state component for online status management

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,23 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 
 import './Contact.css';
 
-function Contact(props) {
-    return (
-        <div className="Contact">
-            <img src={props.avatar} alt={props.name} className="avatar" />
+class Contact extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            online: props.online,
+        }
+    }
+
+    render() {
+        return (
+            <div className="Contact">
+            <img src={this.props.avatar} alt={this.props.name} className="avatar" />
             <div>
-                <h4 className="name">{props.name}</h4>
+                <h4 className="name">{this.props.name}</h4>
                 <div className="status">
-                    <span className={props.online? "status-online" : "status-offline" }/>
-                    <span className=".status-text">{props.online? 'online' : 'offline'}</span>
+                    <span className={this.state.online? "status-online" : "status-offline" }/>
+                    <span className=".status-text">{this.state.online? 'online' : 'offline'}</span>
                 </div>
             </div>
-        </div>
-        
-    )
+        </div> 
+        )
+    }
 }
+
+// function Contact(props) {
+//     return (
+//         <div className="Contact">
+//             <img src={props.avatar} alt={props.name} className="avatar" />
+//             <div>
+//                 <h4 className="name">{props.name}</h4>
+//                 <div className="status">
+//                     <span className={props.online? "status-online" : "status-offline" }/>
+//                     <span className=".status-text">{props.online? 'online' : 'offline'}</span>
+//                 </div>
+//             </div>
+//         </div>
+        
+//     )
+// }
 
 Contact.propTypes = {
     avatar: PropTypes.string.isRequired,
