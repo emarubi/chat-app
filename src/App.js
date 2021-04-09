420b3a09fb56f1889e3750d6ccb96b9df90e9c4d
emarubi <69473685+emarubi@users.noreply.github.com>
2021-04-09 14:08:57 +0200

    new branch contact-list: add new component ContactList with map on Contact component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
-import Contact from './components/Contact'
+import ContactList from './components/ContactList'
 
 function App() {
   return (
     <div className="App">
-        <Contact 
-          name="Elmer Lee"
-          avatar="https://randomuser.me/api/portraits/men/31.jpg"
-          online/>
-          <Contact 
-          name="Joan Porter"
-          avatar="https://randomuser.me/api/portraits/women/87.jpg"
-          />
-          <Contact 
-          name="Gilbert Crawford"
-          avatar="https://randomuser.me/api/portraits/men/36.jpg"
-          online/>
+        <ContactList />
     </div>
   );
 }
